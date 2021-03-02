a46ad3101a408163c3891a0133e7956804f8ce33
peter-keller <keller.f.peter@gmail.com>
2021-03-02 23:00:25 +0000

    feat: more example buttons to display every scenario

--- dev/serve.vue
+++ dev/serve.vue
@@ -8,6 +8,28 @@ export default Vue.extend({
 
 <template>
   <div id="app">
-    <v-button />
+    <div class="buttons">
+      <VButton type="a" color="secondary" :outline="true" href="#" />
+      <VButton type="a" color="secondary" :outline="false" href="#"/>
+      <VButton type="a" color="primary" :outline="true" href="#" />
+      <VButton type="a" color="primary" :outline="false" href="#" />
+
+      <VButton type="button" color="secondary" :outline="true"  />
+      <VButton type="button" color="secondary" :outline="false" />
+      <VButton type="button" color="primary" :outline="true" />
+      <VButton type="button" color="primary" :outline="false" />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#app {
+  background-color: #0f1923;
+}
+
+.buttons {
+  padding: 100px 0;
+  margin: 0 auto;
+  width: 500px;
+}
+</style>
